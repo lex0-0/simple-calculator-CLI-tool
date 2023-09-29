b7e55038adef78e33413da6b9c8cecb00e702f75
Leul Mekonnen <lo.ow8120@gmail.com>
2023-09-29 19:21:47 +0300

    nothing here

--- cal.go
+++ cal.go
@@ -20,33 +20,34 @@ func main() {
 
 	switch operator {
 	case "+":
-		fmt.Printf("%.3f %s %.3f = %.3f", number1, operator, number2, number1+number2)
+		fmt.Printf("%.3f %s %.3f \n = %.3f", number1, operator, number2, number1+number2)
 
 	case "-":
-		fmt.Printf("%.3f %s %.3f = %.3f", number1, operator, number2, number1-number2)
+		fmt.Printf("%.3f %s %.3f \n = %.3f", number1, operator, number2, number1-number2)
 
 	case "*":
-		fmt.Printf("%.3f %s %.2f = %.3f", number1, operator, number2, number1*number2)
+		fmt.Printf("%.3f %s %.2f \n = %.3f", number1, operator, number2, number1*number2)
 	case "/":
 		if number2 == 0.0 {
 			fmt.Println("Divide by zero situation")
 		} else {
-			fmt.Printf("%.3f %s %.3f = %.3f", number1, operator, number2, number1/number2)
+			fmt.Printf("%.3f %s %.3f \n = %.3f", number1, operator, number2, number1/number2)
 
 		}
 	default:
 		fmt.Println("Invalid operator")
 	}
-	fmt.Println(`| |                        | |")
-				| |________________________| |")
-				|     ___ ___ ___   ___      |")
-				|    | 7 | 8 | 9 | | + |     |")
-				|    |___|___|___| |___|     |")
-				|    | 4 | 5 | 6 | | - |     |")
-				|    |___|___|___| |___|     |")
-				|    | 1 | 2 | 3 | | x |     |")
-				|    |___|___|___| |___|     |")
-				|    | . | 0 | = | | / |     |")
-				|    |___|___|___| |___|     |")
-				|____________________________|`)
+
+	fmt.Println(`
+						| |________________________| |)
+						|     ___ ___ ___   ___      |)
+						|    | 7 | 8 | 9 | | + |     |)
+						|    |___|___|___| |___|     |)
+						|    | 4 | 5 | 6 | | - |     |)
+						|    |___|___|___| |___|     |)
+						|    | 1 | 2 | 3 | | x |     |)
+						|    |___|___|___| |___|     |)
+						|    | . | 0 | = | | / |     |)
+						|    |___|___|___| |___|     |)
+						|____________________________|`)
 }
